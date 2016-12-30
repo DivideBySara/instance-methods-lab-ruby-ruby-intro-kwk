64ee15e732b431ea1608454563836dc602d3181c
Sara J <wisconsinsara@gmail.com>
2016-12-30 05:42:07 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+require_relative "../../classes-and-instances-lab-ruby-ruby-intro-kwk/lib/dog"
+require_relative "../../classes-and-instances-lab-ruby-ruby-intro-kwk/lib/person"
+
+class Dog
+
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+
+end # class Dog
